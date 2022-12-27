3af2dad210f3e0c1398250d18d68b6b11672d4e5
Juan Daniel Valencia <112526775+nielvadev@users.noreply.github.com>
2022-12-26 22:35:02 -0500

    Updated to 26/12/2022

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const header = document.getElementById('header');
+const photo = document.getElementById('banner-img');
+const finanzapp = document.getElementById('finanzapp');
+const crmclientes = document.getElementById('crmclientes');
+const veterinapp = document.getElementById('veterinapp');
+const movil = document.getElementById('movil');
+const contacto = document.getElementById('contacto');
 
 let lastScrollY = 0;
 
 window.addEventListener('scroll', () => {
     let currentScrollY = window.scrollY;
+    let position = photo.getBoundingClientRect().top;
+    console.log(position);
 
+    // Animation for the header banner
     if (currentScrollY > 1) {
         if (currentScrollY > lastScrollY) {
             header.classList.add('header--scroll-down');
@@ -19,4 +28,35 @@ window.addEventListener('scroll', () => {
     } else {
         header.classList.remove('header--scroll-up');
     }
+
+    // Animation for the projects -> finanzapp
+    if (position < -900) {
+        finanzapp.classList.remove('project-nodisplay');
+        finanzapp.classList.add('project');
+    }
+
+    // Animation for the projects -> crmclientes
+    if (position < -1700) {
+        crmclientes.classList.remove('project-nodisplay');
+        crmclientes.classList.add('project');
+    }
+
+    // Animation for the projects -> veterinapp
+    if (position < -2300) {
+        veterinapp.classList.remove('project-nodisplay');
+        veterinapp.classList.add('project');
+    }
+
+    // Animation for the projects -> movil
+    if (position < -3000) {
+        movil.classList.remove('project-nodisplay');
+        movil.classList.add('project');
+    }
+
+    // Animation for the contacto
+    if (position < -3320) {
+        contacto.classList.remove('contacto-nodisplay');
+        contacto.classList.add('contacto');
+    }
 });
+
